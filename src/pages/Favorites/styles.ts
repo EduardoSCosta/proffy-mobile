7f1d8684242b3ef3062640609326a664c09354ec
EduardoSCosta <edu_costaaraujo@hotmail.com>
2020-08-07 19:57:53 -0300

    Proffys and Favorites pages

--- src/pages/Favorites/styles.ts
+++ src/pages/Favorites/styles.ts
@@ -3,8 +3,12 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F0F0F7',
+    backgroundColor: '#F0F0F7'
    },
+
+   teacherlist: {
+    marginTop: -40
+  }
 })
 
 export default styles;
